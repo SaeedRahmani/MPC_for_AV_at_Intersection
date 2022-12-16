850229679af1c29ec889875e6583b7c4a372a7a1
Christiaan Theunisse <ctheunisse@tudelft.nl>
2022-12-16 07:47:09 +0100

    Added a second approach to create the motion primitives

--- ro47005-project/visualise_mp.py
+++ ro47005-project/visualise_mp.py
@@ -10,10 +10,24 @@ plt.figure()
 plt.axis('equal')
 plt.ylim(0, 1)
 filenames = ['data/motion_primitives/1.0_1.0_0.0.json', 'data/motion_primitives/1.0_1.0_0.2.json', 'data/motion_primitives/1.0_1.0_0.4.json']
+filenames2 = ['data/motion_primitives/approach21.0_1.0_0.0.json', 'data/motion_primitives/approach21.0_1.0_0.2.json', 'data/motion_primitives/approach21.0_1.0_0.4.json']
+# filenames = ['data/motion_primitives/approach21.0_1.0_0.0.json',
+#              'data/motion_primitives/approach21.0_1.0_0.2.json',
+#              'data/motion_primitives/approach21.0_1.0_0.4.json',
+#              'data/motion_primitives/1.0_1.0_0.0.json',
+#              'data/motion_primitives/1.0_1.0_0.2.json',
+#              'data/motion_primitives/1.0_1.0_0.4.json']
+
 for filename in filenames:
     points = open_json(filename)['points']
     points = np.array(points)
-    plt.plot(points[:, 1], points[:, 0])
-    plt.plot(-points[:, 1], points[:, 0])
+    plt.plot(points[:, 1], points[:, 0], color='b')
+    # plt.plot(-points[:, 1], points[:, 0])
+
+for filename in filenames2:
+    points = open_json(filename)['points']
+    points = np.array(points)
+    plt.plot(points[:, 1], points[:, 0], color='g')
+    # plt.plot(-points[:, 1], points[:, 0])
 
 plt.show()
