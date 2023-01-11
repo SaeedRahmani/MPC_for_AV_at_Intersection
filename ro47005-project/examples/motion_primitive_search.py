71e640267f5329a29ce225931100a99ea63c5531
Jan Neumann <jan.neumann@oracle.com>
2023-01-11 17:13:08 +0100

    Add MPC code

--- ro47005-project/examples/motion_primitive_search.py
+++ ro47005-project/examples/motion_primitive_search.py
@@ -2,12 +2,13 @@ from matplotlib import pyplot as plt
 
 from envs.t_intersection import t_intersection
 from lib.car_dimensions import PriusDimensions, CarDimensions
+from lib.model_predictive_speed_and_steer_control import main
 from lib.motion_primitive import load_motion_primitives
 from lib.motion_primitive_search import MotionPrimitiveSearch
 from lib.plotting import draw_scenario, draw_astar_search_points
 
 if __name__ == '__main__':
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
     for version in ['prius', 'bicycle_model']:
         mps = load_motion_primitives(version=version)
         scenario = t_intersection()
@@ -15,28 +16,14 @@ if __name__ == '__main__':
 
         search = MotionPrimitiveSearch(scenario, car_dimensions, mps, margin=car_dimensions.radius)
 
-
-        draw_scenario(scenario, mps, car_dimensions, search, ax,
-                      draw_obstacles=True, draw_goal=True, draw_car=True, draw_mps=False, draw_collision_checking=False,
-                      draw_car2=False, draw_mps2=False, mp_name='right1')
-
-        # Perform The Search:
-
         try:
             cost, path, trajectory = search.run(debug=True)
 
             cx = trajectory[:, 0]
             cy = trajectory[:, 1]
-            # cyaw = trajectory[:, 2]
+            cyaw = trajectory[:, 2]
             # sp = np.ones_like(cyaw) * 8.3
 
-            # Draw trajectory
-            ax.plot(cx, cy, color='b')
+            main(cx / 0.3, cy / 0.3, cyaw)
         except KeyboardInterrupt:
             pass  # Break The Search On Keyboard Interrupt
-
-        # Draw All Search Points
-        draw_astar_search_points(search, ax, visualize_heuristic=True, visualize_cost_to_come=False)
-
-    ax.axis('equal')
-    plt.show()
