7c9828f9467325a3f9824b99b1cbc5c64f12ecde
lsuryana <lucaselbert@gmail.com>
2023-01-11 12:10:33 +0100

    revision of bicycle model

--- ro47005-project/bicycle/main.py
+++ ro47005-project/bicycle/main.py
@@ -10,15 +10,10 @@ class Bicycle():
     def __init__(self):
         self.xc = 0
         self.yc = 0
-        self.theta = np.pi / 2
+        self.theta = 0
         self.delta = 0
-        self.a = 0
 
         self.L = 2
-        self.lr = 1.2
-        self.w_max = 1.22
-
-        self.sample_time = 0.01
 
     def reset(self):
         self.xc = 0 # vehicle x position
