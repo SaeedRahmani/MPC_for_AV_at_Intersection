db8c4331aaddf25ec800e14acc1f88a71e1e3eaf
Saeed Rahmani <saeedrmd@gmail.com>
2024-06-02 03:06:57 +0200

    chore: use multi-lane search for roundabout

--- main/planner/motion_primitive_search_roundabout.py
+++ main/planner/motion_primitive_search_roundabout.py
@@ -10,11 +10,13 @@ from lib.car_dimensions import BicycleModelDimensions, CarDimensions
 from lib.helpers import measure_time
 from lib.motion_primitive import load_motion_primitives
 #from lib.motion_primitive_search import MotionPrimitiveSearch
-from lib.motion_primitive_search_roundabout import MotionPrimitiveSearch
+# from lib.motion_primitive_search_roundabout import MotionPrimitiveSearch
+from lib.motion_primitive_search_multi_lane import MotionPrimitiveSearch
 from lib.plotting import draw_scenario, draw_astar_search_points
 
 if __name__ == '__main__':
     fig, ax = plt.subplots()
+    # fig, ax = plt.subplots(figsize=(10, 7))
     
     #Scenario Parameters
     start_pos=1
@@ -28,7 +30,7 @@ if __name__ == '__main__':
         search = MotionPrimitiveSearch(scenario, car_dimensions, mps, margin=car_dimensions.radius)
 
         draw_scenario(scenario, mps, car_dimensions, search, ax,
-                      draw_obstacles=True, draw_goal=True, draw_car=True, draw_mps=True, draw_collision_checking=False,
+                      draw_obstacles=True, draw_goal=True, draw_car=True, draw_mps=False, draw_collision_checking=False,
                       draw_car2=False, draw_mps2=False, mp_name='right1')
 
 
@@ -79,7 +81,6 @@ if __name__ == '__main__':
                               label='Visited points A*', markersize=marker_size)
 
     plt.legend(handles=[trajectory, goal_area, goal_direction, obstacles, ego_vehicle, forbidden, mp_search])
-
 
     ax.axis('equal')
     plt.show()
