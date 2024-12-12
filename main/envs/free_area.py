4ab3d0a52348d974740767139f80372b16201ae7
Saeed Rahmani <saeedrmd@gmail.com>
2024-12-12 13:18:59 +0100

    removed plotting from free area

--- main/envs/free_area.py
+++ main/envs/free_area.py
@@ -4,7 +4,7 @@ sys.path.append('..')
 import numpy as np
 from matplotlib import pyplot as plt
 
-from lib.obstacles import BoxObstacle, CircleObstacle
+from lib.obstacles import BoxObstacle, CircleObstaclels
 from lib.scenario import Scenario
 
 def free_area(test_no = 1, angle: float = 0.0, start_pos: float = 0.0, goal_distance = 20, acceptable_error = np.pi / 16) -> Scenario:
@@ -38,60 +38,6 @@ def free_area(test_no = 1, angle: float = 0.0, start_pos: float = 0.0, goal_dist
         obstacles=obstacles,
     )
     
-# def plot_free_area():
-    # Initialize the figure and axis
-    import matplotlib.pyplot as plt
-    from matplotlib.patches import Rectangle, Circle
-
-    fig, ax = plt.subplots(figsize=(10, 10))      
-    # Function to add a box obstacle with label
-    def add_box(center, width, height, label=None, color='gray'):
-        lower_left_corner = (center[0] - width / 2, center[1] - height / 2)
-        ax.add_patch(Rectangle(lower_left_corner, width, height, color=color))
-        if label:
-            ax.text(center[0], center[1], label, ha='center', va='center')
-
-    # Function to add a circle obstacle with label
-    def add_circle(center, radius, label=None, color='gray'):
-        ax.add_patch(Circle(center, radius, color=color))
-        if label:
-            ax.text(center[0], center[1], label, ha='center', va='center')
-
-    # Add the medians
-    add_box((0, -(length / 2 + distance_center)), width_traffic_island, length, 'South Median')  
-    add_circle((0, -distance_center), width_traffic_island / 2)
-    add_box((0, (length / 2 + distance_center)), width_traffic_island, length, 'North Median')  
-    add_circle((0, distance_center), width_traffic_island / 2)
-    add_box((-(length / 2 + distance_center), 0), length, width_traffic_island, 'West Median')  
-    add_circle((-distance_center, 0), width_traffic_island / 2)
-    add_box(((length / 2 + distance_center), 0), length, width_traffic_island, 'East Median')  
-    add_circle((distance_center, 0), width_traffic_island / 2)
-
-    # Add the corners
-    add_circle((-distance_center, -distance_center), distance_center - width_traffic_island / 2 - width_road, 'SW Corner')  
-    add_circle((-distance_center, distance_center), distance_center - width_traffic_island / 2 - width_road, 'NW Corner')  
-    add_circle((distance_center, distance_center), distance_center - width_traffic_island / 2 - width_road, 'NE Corner')  
-    add_circle((distance_center, -distance_center), distance_center - width_traffic_island / 2 - width_road, 'SE Corner')  
-
-    # Add the pavements
-    add_box((-(width_traffic_island / 2 + width_road + width_pavement / 2), -(length / 2 + distance_center)), width_pavement, length)  # south
-    add_box(((width_traffic_island / 2 + width_road + width_pavement / 2), -(length / 2 + distance_center)), width_pavement, length)  # south
-    add_box((-(length / 2 + distance_center), -(width_traffic_island / 2 + width_road + width_pavement / 2)), length, width_pavement)  # west
-    add_box((-(length / 2 + distance_center), (width_traffic_island / 2 + width_road + width_pavement / 2)), length, width_pavement)  # west
-    add_box((-(width_traffic_island / 2 + width_road + width_pavement / 2), (length / 2 + distance_center)), width_pavement, length)  # north
-    add_box(((width_traffic_island / 2 + width_road + width_pavement / 2), (length / 2 + distance_center)), width_pavement, length)  # north
-    add_box(((length / 2 + distance_center), -(width_traffic_island / 2 + width_road + width_pavement / 2)), length, width_pavement)  # east
-    add_box(((length / 2 + distance_center), (width_traffic_island / 2 + width_road + width_pavement / 2)), length, width_pavement)  # east
-
-    # Set the aspect of the plot to be equal
-    ax.set_aspect('equal', 'box')
-
-    # Set the plot limits to focus on the intersection
-    ax.set_xlim(-50, 50)
-    ax.set_ylim(-50, 50)
-
-    # Show the plot
-    plt.show()
 
 # If you want to see how it looks like, uncomment the following and execute the py file
 if __name__ == '__main__':
